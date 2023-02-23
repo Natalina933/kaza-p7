481ab745ee017a1576d7fc5b229a4808b9bcf8c2
Nathalie <nat933@free.fr>
2023-02-23 18:16:13 +0100

    creation : router app home about

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import PublicRouter from './pages/PublicRouter'
+
+function App() {
+  return (
+<BrowserRouter>
+<PublicRouter/>
+</BrowserRouter>
+    )
+}
+
+export default App
