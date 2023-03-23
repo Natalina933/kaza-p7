7139951c3bafac24035f28b69b258505383aff2c
NathHome <nat933@free.fr>
2023-03-23 21:38:29 +0100

    Modif : page lodging

--- src/pages/Lodging/Lodging.jsx
+++ src/pages/Lodging/Lodging.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ImageC from "../../assets/bgHome2.jpg";
+// import ImageC from "../../assets/bgHome2.jpg";
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap');
 </style>
@@ -8,15 +8,15 @@ function lodging({ apparts }) {
   console.log(apparts);
   return (
     <div className="wapperLodging">
-      <div>
-        <img className="imageC" src={ImageC} alt="image" />
+      <div className="logoFooter">
+        {/* <img className="imageC" src={ImageC} alt="image" /> */}
       </div>
       <h1>Paris center,</h1>
       <h2>Paris, ile de france</h2>
-      <ul >
-        <li className="lodging__tag" ><h3>Cosi</h3></li>
-        <li className="lodging__tag"><h3>Canal</h3></li>
-        <li className="lodging__tag"><h3>Paris 10</h3></li>
+      <ul className="lodging__tag" >
+        <li ><h3>Cosi</h3></li>
+        <li ><h3>Canal</h3></li>
+        <li ><h3>Paris 10</h3></li>
       </ul>
 
       <div className="host__lodging">
