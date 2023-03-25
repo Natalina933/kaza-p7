e26da16cd279b8dfc7a21fe1e1c7daf3d8afa7ef
Nathalie <nat933@free.fr>
2023-03-25 19:47:58 +0100

    modifications : carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import leftarrow from "../../assets/leftarrow.svg"
-import rightarrow from "../../assets/rightarrow.svg"
+import React, { useState } from "react";
+import leftarrow from "../../assets/leftarrow.svg";
+import rightarrow from "../../assets/rightarrow.svg";
 
-function Carousel({appartPictures}) {
-    console.log(appartPictures);
+function Carousel({ appartPictures }) {
+  const [indexImage, setIndexImage] = useState(0);
+  function backImage() {
+    setIndexImage(
+      (indexImage + appartPictures.length + 1) % appartPictures.length
+    );
+  }
+  function nextImage() {
+    setIndexImage((indexImage + 1) % appartPictures.length);
+  }
   return (
-    <div className='carousel'>
-        <img className='leftarrow' src={leftarrow} alt="/" />
-        <img src={appartPictures[0]} alt="" />
-        <img className='rightarrow' src={rightarrow} alt="/" />
+    <div className="carousel">
+      <img className="leftarrow" src={leftarrow} alt="/" onClick={backImage} />
+      <img src={appartPictures[indexImage]} alt="" />
+      <img
+        className="rightarrow"
+        src={rightarrow}
+        alt="/"
+        onClick={nextImage}
+      />
+      <p>{indexImage+1}/{appartPictures.length}</p>
+
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
