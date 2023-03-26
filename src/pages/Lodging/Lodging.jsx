6cdb394d1b9b02c6a607f073a2005518467f457b
NathHome <nat933@free.fr>
2023-03-26 21:25:25 +0200

    Modifications : lodging

--- src/pages/Lodging/Lodging.jsx
+++ src/pages/Lodging/Lodging.jsx
@@ -3,15 +3,30 @@ import { useParams } from 'react-router-dom';
 import Carousel from '../../components/Carousel/Carousel';
 
 function Lodging({apparts}) {
- const {lodgingId}=useParams()
- const appart=apparts.find((appart)=>appart.id===lodgingId)
+  const {lodgingId}=useParams()
+  const appart=apparts.find((appart)=>appart.id===lodgingId)
 console.log(appart);
 
   return (
-    <div className='wapperLodging'>
-      <Carousel appartPictures={appart.pictures}/> 
-    </div>
-  )
+    <><div className='wapperLodging'>
+      <Carousel appartPictures={appart.pictures} />
+    </div><div className="lodging__title">
+        <h1>{appart.title}</h1>
+        <h2>{appart.tags}</h2>
+      </div>
+      <div className="lodging__host">
+        {/* <h2>{appart.host}</h2> */}
+        <img src={appart.picture} alt={appart.title} />
+        <div className='lodging__stars'></div>
+      </div>
+      <div className="lodging__description">
+        <p>{appart.description}</p>
+      </div>
+      <div className="lodging__equipement">
+        <p>{appart.equipements}</p>
+      </div>
+      </>
+)
 }
 
 export default Lodging
