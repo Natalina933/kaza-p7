5fd8f3d17f52432116ba3a50eab4affcbb6c78e8
Nathalie <nat933@free.fr>
2023-03-27 19:45:14 +0200

    Modif Style : lodging page

--- src/pages/Lodging/Lodging.jsx
+++ src/pages/Lodging/Lodging.jsx
@@ -8,23 +8,38 @@ function Lodging({apparts}) {
 console.log(appart);
 
   return (
-    <><div className='wapperLodging'>
-      <Carousel appartPictures={appart.pictures} />
-    </div><div className="lodging__title">
-        <h1>{appart.title}</h1>
-        <h2>{appart.tags}</h2>
-      </div>
-      <div className="lodging__host">
-        {/* <h2>{appart.host}</h2> */}
-        <img src={appart.picture} alt={appart.title} />
-        <div className='lodging__stars'></div>
-      </div>
-      <div className="lodging__description">
-        <p>{appart.description}</p>
-      </div>
-      <div className="lodging__equipement">
-        <p>{appart.equipements}</p>
-      </div>
+    <><div className="lodging__page">
+      <div className='lodging__carousel'>
+        <Carousel appartPictures={appart.pictures} />
+      </div><div className="lodging__title">
+          <h1>{appart.title}</h1>
+          <h2>{appart.location}</h2>
+        </div>
+        <div className="lodging__host">
+          <div className="lodging__tags">
+            <ol>
+              <li>Cosy</li>
+              <li>Canal</li>
+              <li>Paris 10</li>
+            </ol>
+          </div>
+          <h2>{appart.host.name}</h2>
+          <div className='lodging__hostImg'><img src={appart.host.picture} alt={"Photo de "+ appart.title} /></div>
+      
+          <div className='lodging__stars'></div>
+          <div>étoile</div>
+          <div>étoile</div>
+          <div>étoile</div>
+          <div>étoile</div>
+          <div>étoile</div>
+        </div>
+        <div className="lodging__description">
+          <p>{appart.description}</p>
+        </div>
+        <div className="lodging__equipement">
+          <p>{appart.equipements}</p>
+        </div>
+    </div>
       </>
 )
 }
