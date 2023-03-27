630f2b81cf78ab8397d1c52328e6bd1149db6adb
Nathalie <nat933@free.fr>
2023-03-27 20:01:43 +0200

    Modif Style : tags

--- src/pages/Lodging/Lodging.jsx
+++ src/pages/Lodging/Lodging.jsx
@@ -11,35 +11,39 @@ console.log(appart);
     <><div className="lodging__page">
       <div className='lodging__carousel'>
         <Carousel appartPictures={appart.pictures} />
-      </div><div className="lodging__title">
-          <h1>{appart.title}</h1>
-          <h2>{appart.location}</h2>
-        </div>
-        <div className="lodging__host">
-          <div className="lodging__tags">
-            <ol>
-              <li>Cosy</li>
-              <li>Canal</li>
-              <li>Paris 10</li>
-            </ol>
-          </div>
-          <h2>{appart.host.name}</h2>
-          <div className='lodging__hostImg'><img src={appart.host.picture} alt={"Photo de "+ appart.title} /></div>
+      </div>
       
-          <div className='lodging__stars'></div>
-          <div>étoile</div>
-          <div>étoile</div>
-          <div>étoile</div>
-          <div>étoile</div>
-          <div>étoile</div>
-        </div>
-        <div className="lodging__description">
-          <p>{appart.description}</p>
-        </div>
-        <div className="lodging__equipement">
-          <p>{appart.equipements}</p>
-        </div>
-    </div>
+      <div className="lodging__wrapper">
+        <div className="lodging__title">
+            <h1>{appart.title}</h1>
+            <h2>{appart.location}</h2>
+          </div>
+          <div className="lodging__host">
+            <div className="lodging__tags">
+              <ol>
+                <li>Cosy</li>
+                <li>Canal</li>
+                <li>Paris 10</li>
+              </ol>
+            </div>
+            <h2>{appart.host.name}</h2>
+            <div className='lodging__hostImg'><img src={appart.host.picture} alt={"Photo de "+ appart.title} /></div>
+        
+            <div className='lodging__stars'></div>
+            <div>étoile</div>
+            <div>étoile</div>
+            <div>étoile</div>
+            <div>étoile</div>
+            <div>étoile</div>
+          </div>
+          <div className="lodging__description">
+            <p>{appart.description}</p>
+          </div>
+          <div className="lodging__equipement">
+            <p>{appart.equipements}</p>
+          </div>
+            </div>
+      </div>
       </>
 )
 }
