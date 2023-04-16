d9e90d62187daf2613518d38e449b2e4d3b5b8e2
Nathalie <nat933@free.fr>
2023-04-16 16:31:55 +0200

    Modification style : info host lodging

--- src/components/InfosAppart/InfosHost.jsx
+++ src/components/InfosAppart/InfosHost.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import Stars from "./Stars";
 
 function InfosHost({ appart }) {
-console.log(appart);
-return (
-<>
-<div className="lodging__wrapperHost">
-<div className="lodging__host">
-<div className="lodging__stars">
-<Stars rating={appart.rating} />
-</div>
-</div>
-<h2>{appart.host.name}</h2>
-<div className="lodging__hostImg">
-<img src={appart.host.picture} alt={"Photo de " + appart.title} />
-</div>
-</div>
-</>
-);
+    console.log(appart);
+    return (
+    <>
+            <div className="lodging__host">
+                <div className="lodging__stars">
+                <Stars rating={appart.rating} />
+                </div>
+            </div>
+            <div className="lodging__hostInfos">
+                <h2>{appart.host.name}</h2>
+                <div className="lodging__hostImg">
+                <img src={appart.host.picture} alt={"Photo de " + appart.title} />
+                </div>
+            </div>
+    </>
+    );
 }
 
 export default InfosHost;
