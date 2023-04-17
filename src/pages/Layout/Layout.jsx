ee12c1483bd974784eb890e4514f6ce586e592a5
Nathalie <nat933@free.fr>
2023-04-17 20:32:59 +0200

    Modifications : page about

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import Footer from '../../components/Footer/Footer'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 //relie mes pages
 function Layout() {
   return (
     <>
-    <Navbar/>
-    <Outlet />
-    <Footer />
+      <div className="site-wrapper">
+        <Navbar />
+        <Outlet />
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
