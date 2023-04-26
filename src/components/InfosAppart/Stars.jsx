ec7909702639683cabb96693940a60a7f8bb74b3
Nathalie <nat933@free.fr>
2023-04-26 15:05:01 +0200

    Correction stars info lodging

--- src/components/InfosAppart/Stars.jsx
+++ src/components/InfosAppart/Stars.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
-import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
 
 function Stars({ rating }) {
-const maxRating = 5; // Limite de 5 étoiles
+const maxRating = 5; 
 const stars = [];
 for (let i = 1; i <= maxRating; i++) {
 if (i <= rating) {
-stars.push(<FontAwesomeIcon key={i} icon={solidStar} className="star" />);
+stars.push(<FontAwesomeIcon key={i} icon={solidStar} className="active-star" />);
 } else {
-stars.push(<FontAwesomeIcon key={i} icon={regularStar} className="star" />);
+stars.push(<FontAwesomeIcon key={i} icon={solidStar} className="inactive-star" />);
 }
 }
 return <div>{stars}</div>;
